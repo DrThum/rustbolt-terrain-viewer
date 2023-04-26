5986262ecefaa57e575ba5a9d97af4f3453ec416
DrThum <thum@drthum.net>
2023-04-26 17:43:54 +0200

    Use one geometry per block (going from 17 to 144+ fps)

--- src/gui/viewport.ts
+++ src/gui/viewport.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import Stats from 'stats.js'
 import { MapControls } from 'three/addons/controls/MapControls.js'
+import { mergeBufferGeometries } from 'three/addons/utils/BufferGeometryUtils.js'
 import constants from '../models/constants'
 import { setup } from './gradient-map'
 import { TerrainGeometry } from '../models/terrain_geometry'
@@ -45,6 +46,7 @@ function animate() {
 
   renderer.render(scene, camera)
 
+  console.log(renderer.info.render)
   stats.end()
 }
 
@@ -77,27 +79,39 @@ const gradientMap = setup()
 export function drawTerrain(
   blockOffsetX: number,
   blockOffsetZ: number,
-  index: number,
-  terrain: TerrainGeometry
+  terrainChunks: TerrainGeometry[]
 ) {
-  const chunkOffsetX = blockOffsetX + constants.CHUNK_WIDTH * (index % 16)
-  const chunkOffsetZ =
-    blockOffsetZ + constants.CHUNK_WIDTH * Math.floor(index / 16)
+  const planes = terrainChunks.map((chunk, index) => {
+    const chunkOffsetX = blockOffsetX + constants.CHUNK_WIDTH * (index % 16)
+    const chunkOffsetZ =
+      blockOffsetZ + constants.CHUNK_WIDTH * Math.floor(index / 16)
 
-  const planeGeom = new THREE.PlaneGeometry(
-    constants.CHUNK_WIDTH,
-    constants.CHUNK_WIDTH,
-    16,
-    16
-  )
-  planeGeom.rotateX(-Math.PI * 0.5)
-
-  for (let i = 0; i < planeGeom.attributes.position.count; i++) {
-    ; (planeGeom.attributes.position as THREE.BufferAttribute).setY(
-      i,
-      terrain.heightMap[i] + terrain.baseHeight
+    const planeGeom = new THREE.PlaneGeometry(
+      constants.CHUNK_WIDTH,
+      constants.CHUNK_WIDTH,
+      16,
+      16
     )
-  }
+    planeGeom.rotateX(-Math.PI * 0.5)
+
+    for (let i = 0; i < planeGeom.attributes.position.count; i++) {
+      ; (planeGeom.attributes.position as THREE.BufferAttribute).setY(
+        i,
+        chunk.heightMap[i] + chunk.baseHeight
+      )
+    }
+
+    planeGeom.translate(chunkOffsetX, 0, chunkOffsetZ)
+
+    const edges = new THREE.EdgesGeometry(planeGeom, 10)
+
+    return {
+      plane: planeGeom,
+      edges: edges
+    }
+  })
+
+  const singleGeometry = mergeBufferGeometries(planes.map(({ plane }) => plane))
 
   const shaderMat = new THREE.ShaderMaterial({
     uniforms: {
@@ -107,21 +121,15 @@ export function drawTerrain(
     vertexShader: identityVertex,
     fragmentShader: heatFragment,
   })
-  const mesh = new THREE.Mesh(planeGeom, shaderMat)
+  const mesh = new THREE.Mesh(singleGeometry, shaderMat)
 
-  const edges = new THREE.EdgesGeometry(planeGeom, 10)
+  const edges = mergeBufferGeometries(planes.map(({ edges }) => edges))
   const edgeMat = shaderMat.clone()
   edgeMat.uniforms.darkeningFactor = { value: 0.9 } // Darken the wireframe mesh
   const line = new THREE.LineSegments(
     edges,
     edgeMat
   )
-
-  mesh.position.x += chunkOffsetX
-  mesh.position.z += chunkOffsetZ
-
-  line.position.x += chunkOffsetX
-  line.position.z += chunkOffsetZ
 
   scene.add(mesh)
   scene.add(line)
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { loadBlock } from './parsing'
 import constants from './models/constants'
-import { drawTerrain } from './gui/viewport'
+import { drawTerrain as buildBlockGeometryFromChunks } from './gui/viewport'
 
 // GUI:
 // - toggle between gradient/area
@@ -13,10 +13,7 @@ for (let blockRow = 29; blockRow < 34; blockRow++) {
     const blockOffsetX = constants.BLOCK_WIDTH * (blockRow - 32)
     const blockOffsetZ = constants.BLOCK_WIDTH * (blockCol - 32)
 
-    const terrains = await loadBlock('Azeroth', blockRow, blockCol)
-
-    terrains.forEach((terrain, idx) => {
-      drawTerrain(blockOffsetX, blockOffsetZ, idx, terrain)
-    })
+    const terrainChunks = await loadBlock('Azeroth', blockRow, blockCol)
+    buildBlockGeometryFromChunks(blockOffsetX, blockOffsetZ, terrainChunks)
   }
 }
