2ad6d7bc19a2f58c8cff6956fd0162a10f20ce3e
DrThum <thum@drthum.net>
2023-04-27 18:17:39 +0200

    Color the ground according to the chunk area id

--- src/gui/viewport.ts
+++ src/gui/viewport.ts
@@ -5,6 +5,8 @@ import { mergeGeometries } from 'three/addons/utils/BufferGeometryUtils.js'
 import constants from '../models/constants'
 import { setup } from './gradient-map'
 import { TerrainGeometry } from '../models/terrain_geometry'
+import { Font } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -53,11 +55,15 @@ function animate() {
 animate()
 
 const identityVertex = `
+  attribute float areaId;
+
   varying float hValue;
+  flat varying float hAreaId;
 
   void main()
   {
     hValue = position.y;
+    hAreaId = areaId;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   }
 `
@@ -65,21 +71,34 @@ const identityVertex = `
 const heatFragment = `
   uniform sampler2D gradientMap;
   uniform float darkeningFactor;
+
   varying float hValue;
+  flat varying float hAreaId;
+
+  vec3 unpackColor(float f)
+  {
+    vec3 color;
+    color.r = floor(f / 65536.);
+    color.g = floor((f - color.r * 65536.) / 256.0);
+    color.b = floor(f - color.r * 65536. - color.g * 256.0);
+    return color / 256.0;
+  }
 
   void main() {
     float v = clamp(hValue / 250., 0., 1.);
-    vec3 col = texture2D(gradientMap, vec2(0, v)).rgb * vec3(darkeningFactor, darkeningFactor, darkeningFactor);
+    // vec3 col = texture2D(gradientMap, vec2(0, v)).rgb * vec3(darkeningFactor);
+    vec3 col = unpackColor(hAreaId * (16777215. / 4130.));
     gl_FragColor = vec4(col, 1.);
   }
 `
 
 const gradientMap = setup()
 
-export function drawTerrain(
+export function buildBlockGeometryFromChunks(
   blockOffsetX: number,
   blockOffsetZ: number,
-  terrainChunks: TerrainGeometry[]
+  terrainChunks: TerrainGeometry[],
+  font: Font
 ) {
   const planes = terrainChunks.map((chunk, index) => {
     const chunkOffsetX = blockOffsetX + constants.CHUNK_WIDTH * (index % 16)
@@ -95,26 +114,47 @@ export function drawTerrain(
     planeGeom.rotateX(-Math.PI * 0.5)
 
     const count = planeGeom.attributes.position.count
-    const areaIdArray = new Uint32Array(count / 3)
-    areaIdArray.fill(0, count / 3, chunk.areaId)
+    const areaIdArray = new Float32Array(count)
+    areaIdArray.fill(chunk.areaId, 0, count)
     const areaIds = new THREE.BufferAttribute(areaIdArray, 1)
+    let maxHeight = -999999
     for (let i = 0; i < planeGeom.attributes.position.count; i++) {
-      ;(planeGeom.attributes.position as THREE.BufferAttribute).setY(
-        i,
-        chunk.heightMap[i] + chunk.baseHeight
-      )
+      const y = chunk.heightMap[i] + chunk.baseHeight
+
+      ;(planeGeom.attributes.position as THREE.BufferAttribute).setY(i, y)
+
+      maxHeight = Math.max(maxHeight, y)
     }
 
     planeGeom.translate(chunkOffsetX, 0, chunkOffsetZ)
     planeGeom.setAttribute('areaId', areaIds)
+
+    const areaIdText = new TextGeometry(chunk.areaId.toString(), {
+      font: font,
+      size: 6,
+      height: 0.5,
+      curveSegments: 3,
+    })
+    areaIdText.rotateX(-Math.PI * 0.25)
+    areaIdText.translate(chunkOffsetX, maxHeight + 20, chunkOffsetZ)
 
     const edges = new THREE.EdgesGeometry(planeGeom, 10)
 
     return {
       plane: planeGeom,
       edges: edges,
+      areaIdText,
     }
   })
+
+  const textSingleGeometry = mergeGeometries(
+    planes.map(({ areaIdText }) => areaIdText)
+  )
+  const textMesh = new THREE.Mesh(
+    textSingleGeometry,
+    new THREE.MeshBasicMaterial({ color: 'black' })
+  )
+  scene.add(textMesh)
 
   const singleGeometry = mergeGeometries(planes.map(({ plane }) => plane))
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { loadBlock } from './parsing'
 import constants from './models/constants'
-import { drawTerrain as buildBlockGeometryFromChunks } from './gui/viewport'
+import { buildBlockGeometryFromChunks } from './gui/viewport'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+
+import fontFile from './fonts/helvetiker_regular.typeface.json'
 
 // GUI:
 // - toggle between gradient/area
@@ -8,14 +11,22 @@ import { drawTerrain as buildBlockGeometryFromChunks } from './gui/viewport'
 // - 2 buttons for specific readable gradients and a third one to generate a random gradient
 // import * as dat from 'dat.gui'
 
-for (let blockRow = 29; blockRow < 35; blockRow++) {
-  for (let blockCol = 29; blockCol < 35; blockCol++) {
+const loader = new FontLoader()
+const font = loader.parse(fontFile)
+
+for (let blockRow = 31; blockRow < 33; blockRow++) {
+  for (let blockCol = 31; blockCol < 33; blockCol++) {
     const blockOffsetX = constants.BLOCK_WIDTH * (blockRow - 32)
     const blockOffsetZ = constants.BLOCK_WIDTH * (blockCol - 32)
 
     const terrainChunks = await loadBlock('Azeroth', blockRow, blockCol)
     if (terrainChunks.length > 0) {
-      buildBlockGeometryFromChunks(blockOffsetX, blockOffsetZ, terrainChunks)
+      buildBlockGeometryFromChunks(
+        blockOffsetX,
+        blockOffsetZ,
+        terrainChunks,
+        font
+      )
     }
   }
 }
